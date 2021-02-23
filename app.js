1b1b50a2844ee53a8b73289b52d9ecf46842e5e8
Gergoe Eperjesi <gergoe.eperjesi@gmail.com>
2021-02-23 11:15:38 +0100

    flip card function

--- app.js
+++ app.js
@@ -1,68 +1,85 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     //card options
     const cardArray = [
-        {
-            name: 'fries',
-            img:'imsges/fries.jpg' 
-        },
-        {
-            name: 'fries',
-            img:'imsges/fries.jpg' 
-        },
-        {
-            name: 'cheeseburger',
-            img:'imsges/cheeseburger.jpg' 
-        },
-        {
-            name: 'cheeseburger',
-            img:'imsges/cheeseburger.jpg' 
-        },
-        {
-            name: 'hotdog',
-            img:'imsges/hotdog.jpg' 
-        },
-        {
-            name: 'hotdog',
-            img:'imsges/hotdog.jpg' 
-        },
-        {
-            name: 'ice-cream',
-            img:'imsges/ice-cream.jpg' 
-        },
-        {
-            name: 'ice-cream',
-            img:'imsges/ice-cream.jpg' 
-        },
-        {
-            name: 'milkshake',
-            img:'imsges/milkshake.jpg' 
-        },
-        {
-            name: 'milkshake',
-            img:'imsges/milkshake.jpg' 
-        },
-        {
-            name: 'pizza',
-            img:'imsges/pizza.jpg' 
-        },
-        {
-            name: 'pizza',
-            img:'imsges/pizza.jpg' 
-        },
+      {
+        name: 'fries',
+        img: 'images/fries.png'
+      },
+      {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+      },
+      {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+      },
+      {
+        name: 'pizza',
+        img: 'images/pizza.png'
+      },
+      {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+      },
+      {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+      },
+      {
+        name: 'fries',
+        img: 'images/fries.png'
+      },
+      {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+      },
+      {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+      },
+      {
+        name: 'pizza',
+        img: 'images/pizza.png'
+      },
+      {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+      },
+      {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+      }
     ]
 
     const grid = document.querySelector('.grid')
+    var cardsChosen = []
+    var cardsChosenId = []
 
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
-            var card = documet.createElement('img')
-            card.setAttribute('src', 'images/blank.jpg')
-            card.setAttribute('data-id', i)
-            // card.eventListener('click', flipcard)
-            grid.appendChild(card)
+          const card = document.createElement('img')
+          card.setAttribute('src', 'images/blank.png')
+          card.setAttribute('data-id', i)
+          // card.addEventListener('click', flipCard)
+          grid.appendChild(card)
         }
-    }
+      }
+
+      //check for matches
+
+
+      // flip card
+      function flipCard() {
+          var cardId = this.getAttribute('data-id')
+          cardsChosen.push(cardArray[cardId].name)
+          cardsChosenId.push(cardId)
+          this.setAttribute('src', cardArray[cardId].img)
+          if (cardsChosen === 2) {
+            setTimeout(checkForMatch, 500)
+          }
+
+      }
+
 
     createBoard()
 
